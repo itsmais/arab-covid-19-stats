98e32f830ffcf7dfedcbeeae2e28dee07a1de06d
Mais <59611735+itsmais@users.noreply.github.com>
2020-08-17 21:43:28 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+const listOfArabCountries =[
+    'algeria',
+    'bahrain',
+    'comoros',
+    'djibouti',
+    'egypt',
+    'iraq',
+    'jordan',
+    'kuwait',
+    'lebanon',
+    'libya',
+    'mauritania',
+    'morocco',
+    'oman',
+    'palestine',
+    'qatar',
+    'saudi-arabia',
+    'somalia',
+    'sudan',
+    'syria',
+    'tunisia',
+    'united-arab-emirates',
+    'yemen'
+]
+
+function makeTableCell(element){
+    return ("<td>" + element + "</td>");
+}
+
+  function makeTableRow(element){
+      return ("<tr>" + element + "</tr>");
+  }
+
+listOfArabCountries.forEach(country => {
+}); 
+
+let table = document.getElementById("stats-table");
+
+
+let settings = {
+    "url": "https://api.covid19api.com/summary",
+    "method": "GET",
+    "timeout": 0,
+  };
+$.ajax(settings).done(function (response) {
+    console.log(response);
+    response.Countries.forEach(countryObject => {
+        let rowContent="";
+        if (listOfArabCountries.indexOf(countryObject.Slug)>=0){
+            rowContent = rowContent + makeTableCell(countryObject.Country); // Country
+            rowContent = rowContent + makeTableCell(countryObject.NewRecovered); // New Recoveries
+            rowContent = rowContent + makeTableCell(countryObject.TotalRecovered); // Total Recoveries
+            rowContent = rowContent + makeTableCell(countryObject.TotalDeaths); // Total Deaths
+            rowContent = rowContent + makeTableCell(countryObject.TotalConfirmed); // Total Confirmed
+            table.innerHTML += makeTableRow(rowContent);
+            // document.getElementById("main-text").innerHTML+=countryObject.Country + " " + countryObject.TotalConfirmed+"</br>";
+        }
+    });
+  });   
